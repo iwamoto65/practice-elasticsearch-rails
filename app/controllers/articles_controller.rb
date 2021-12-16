011159e50a1d748a99482ece1b5e58e64b3ff270
iwamoto65 <kosuke00940@icloud.com>
2021-12-16 21:12:46 +0900

    add search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.order(created_at: :desc)
+    @articles = Article.search_document(params[:title])
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,39 @@ class Article < ApplicationRecord
       indexes :description, analyzer: 'english'
     end
   end
+
+  class << self
+    def search_document(title = nil)
+      if title.present?
+        title_keyword_query = match_title_query(title)
+        __elasticsearch__.search(title_keyword_query)
+      else
+        __elasticsearch__.search(match_all_query)
+      end
+    end
+
+    private
+
+    def match_title_query(title)
+      {
+        query: {
+          query_string: {
+            query: "*#{title}*",
+            fields: [
+              "title"
+            ]
+          }
+        }
+      }
+    end
+
+    def match_all_query
+      {
+        query: {
+          match_all: {}
+        },
+        size: 100
+      }
+    end
+  end
 end
