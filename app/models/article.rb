a91f4930a29ca9525497f97b3cffd6bcc6b527e9
iwamoto65 <kosuke00940@icloud.com>
2021-12-25 13:43:48 +0900

    install annotate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_articles_on_created_at  (created_at)
+#
 class Article < ApplicationRecord
   include Elasticsearch::ArticleSearchable
 end
--- app/models/draft_article.rb
+++ app/models/draft_article.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: draft_articles
+#
+#  id          :bigint           not null, primary key
+#  description :text(65535)
+#  status      :integer          default("draft"), not null
+#  title       :string(255)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class DraftArticle < ApplicationRecord
   enum status: { draft: 0, published: 1 }
 end
